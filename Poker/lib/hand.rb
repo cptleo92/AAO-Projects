cea4c101a3160ba41b60487f435ac4244a8a094a
LeoC <leo.cheng92@gmail.com>
2022-01-12 01:19:49 -0500

    added methods for comparing hands

--- Poker/lib/hand.rb
+++ Poker/lib/hand.rb
@@ -1,14 +1,16 @@
+require "byebug"
 require_relative "deck.rb"
 
 class Hand 
+
+  VALUES = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"] 
 
   attr_reader :deck
   attr_accessor :cards, :score  
 
   def initialize(deck)
     @deck = deck
-    @cards = []
-    @score = 0
+    @cards = []    
   end
 
   def discard(num)
@@ -25,4 +27,88 @@ class Hand
     @cards << @deck.draw!
   end
 
+  def sort!
+    @cards.sort_by! { |card| card.score }
+  end
+
+  def calc_score
+    case 
+    when royal_flush?
+        108
+    when straight_flush?
+        107
+    when four_kind?
+        106
+    when full_house?
+        105
+    when flush?
+        104
+    when straight?
+        103
+    when triplet?
+        102
+    when two_pair?
+        101
+    when pair?
+        100
+    else
+        high_card.score
+    end
+  end
+
+  def high_card    
+    highest = @cards.inject do |card1, card2|
+      card1.score > card2.score ? card1 : card2
+    end
+    highest
+  end
+
+  def royal_flush?
+    high_card.value == "A" && flush?
+  end
+
+  def straight_flush?
+    straight? && flush?
+  end
+
+  def four_kind?
+    counter = Hash.new(0)
+    @cards.each { |card| counter[card.value] += 1 }
+    counter.any? { |val, count| count == 4}
+  end
+
+  def full_house?
+    triplet? && pair?
+  end
+
+  def flush?
+    suit = @cards[0].suit
+    @cards.all? { |card| card.suit == suit}
+  end
+
+  def straight?
+    sort!
+    (0..3).each do |idx|
+      return false unless @cards[idx].score == @cards[idx+1].score - 1       
+    end
+    true
+  end
+
+  def triplet?
+    counter = Hash.new(0)
+    @cards.each { |card| counter[card.value] += 1 }
+    counter.any? { |val, count| count == 3}
+  end
+
+  def two_pair?
+    counter = Hash.new(0)
+    @cards.each { |card| counter[card.value] += 1 }
+    counter.count { |val, count| count == 2} == 2
+  end
+
+  def pair?
+    counter = Hash.new(0)
+    @cards.each { |card| counter[card.value] += 1 }
+    counter.one? { |val, count| count == 2}
+  end
 end
--- Poker/spec/hand_spec.rb
+++ Poker/spec/hand_spec.rb
@@ -16,9 +16,9 @@ describe Hand do
      expect(hand.cards).to eq([]) 
    end
 
-   it "has a hand score" do
-     expect(hand.score).to eq(0)
-   end      
+#    it "has a hand score" do
+#      expect(hand.score).to eq(0)
+#    end      
   end 
 
   describe "#discard" do
@@ -44,46 +44,160 @@ describe Hand do
     end
   end  
 
-  describe "#calc_score" do
-    it "checks for any combinations"
+  describe "#sort!" do 
+    let(:card1) { Card.new("A", :H)}
+    let(:card2) { Card.new("10", :H)}
+    let(:card3) { Card.new("J", :H)}
+    let(:card4) { Card.new("7", :H)}    
+    let(:card5) { Card.new("4", :H)}   
+    let(:h) { Hand.new(deck) }
+    it "sorts hand from lowest to highest" do
+      h.cards = [card1, card2, card3, card4, card5]
+      h.sort!
+      expect(h.cards).to eq([card5, card4, card2, card3, card1])
+    end
+  end
 
-    it "should return integer score value" 
+  describe "#high_card" do
+    let(:card1) { Card.new("4", :H)}
+    let(:card2) { Card.new("K", :H)}
+    let(:card3) { Card.new("J", :H)}
+    let(:card4) { Card.new("7", :H)}    
+    let(:h) { Hand.new(deck) }
+    it "returns score of highest card in hand" do
+      h.cards = [card1, card2, card3, card4]
+      expect(h.high_card).to eq(card2)
+    end
+  end
+
+  describe "#calc_score" do
+    let(:card1) { Card.new("K", :H)}
+    let(:card2) { Card.new("A", :H)}
+    let(:card3) { Card.new("A", :D)}
+    let(:card4) { Card.new("A", :H)}
+    let(:card5) { Card.new("J", :H)}       
+    it "checks for any combinations" do
+      hand.cards = [card1, card2, card3, card4, card5]
+      expect(hand.royal_flush?).to be true
+    end
+    it "should return integer score value" do
+      hand.cards = [card1, card2, card3, card4, card5]
+      expect(hand.calc_score).to eq(108)
+    end
   end
 
   describe "#royal_flush?" do
-    it "checks for a royal flush"
+    let(:card1) { Card.new("K", :H)}
+    let(:card2) { Card.new("10", :H)}
+    let(:card3) { Card.new("A", :H)}
+    let(:card4) { Card.new("Q", :H)}
+    let(:card5) { Card.new("J", :H)}        
+    # let(:h) { Hand.new(deck) }
+    it "checks for a royal flush" do
+        hand.cards = [card1, card2, card3, card4, card5]
+        expect(hand.royal_flush?).to be true
+    end
   end
 
   describe "#straight_flush?" do
-    it "checks for a straight flush"
+    let(:card1) { Card.new("K", :H)}
+    let(:card2) { Card.new("10", :H)}
+    let(:card3) { Card.new("9", :H)}
+    let(:card4) { Card.new("Q", :H)}
+    let(:card5) { Card.new("J", :H)}        
+    let(:h) { Hand.new(deck) }
+    it "checks for a straight flush" do
+      h.cards = [card1, card2, card3, card4, card5]
+      expect(h.straight_flush?).to be true
+    end
   end
 
   describe "#four_kind?" do
-    it "checks for a four of a kind"
+    let(:card1) { Card.new("4", :H)}
+    let(:card2) { Card.new("4", :H)}
+    let(:card3) { Card.new("4", :H)}
+    let(:card4) { Card.new("5", :H)}
+    let(:card5) { Card.new("7", :H)}        
+    let(:h) { Hand.new(deck) }
+    it "checks for a four of a kind" do
+      h.cards = [card1, card2, card3, card4, card5]
+      expect(h.four_kind?).to be false
+    end
   end
 
   describe "#full_house?" do
-    it "checks for a full house"
+    let(:card1) { Card.new("4", :H)}
+    let(:card2) { Card.new("4", :H)}
+    let(:card3) { Card.new("4", :H)}
+    let(:card4) { Card.new("5", :H)}
+    let(:card5) { Card.new("5", :H)} 
+    it "checks for a full house" do
+      hand.cards = [card1, card2, card3, card4, card5]
+      expect(hand.full_house?).to be true
+    end
   end
 
   describe "#flush?" do
-    it "checks for a flush"
+    let(:card1) { Card.new("4", :H)}
+    let(:card2) { Card.new("4", :H)}
+    let(:card3) { Card.new("4", :H)}
+    let(:card4) { Card.new("5", :H)}
+    let(:card5) { Card.new("7", :H)}        
+    let(:h) { Hand.new(deck) }
+    it "checks for a flush" do
+      h.cards = [card1, card2, card3, card4, card5]
+      expect(h.flush?).to be true
+    end
   end
 
   describe "#straight?" do
-    it "checks for a straight"
+    let(:card1) { Card.new("4", :H)}
+    let(:card2) { Card.new("8", :H)}
+    let(:card3) { Card.new("6", :S)}
+    let(:card4) { Card.new("5", :D)}
+    let(:card5) { Card.new("7", :H)}        
+    let(:h) { Hand.new(deck) }
+    it "checks for a straight" do
+      h.cards = [card1, card2, card3, card4, card5]
+      expect(h.straight?).to be true
+    end
   end
 
   describe "#triplet?" do
-    it "checks for a triplet"
+    let(:card1) { Card.new("4", :H)}
+    let(:card2) { Card.new("4", :H)}
+    let(:card3) { Card.new("4", :H)}
+    let(:card4) { Card.new("8", :H)}
+    let(:card5) { Card.new("5", :H)}        
+    it "checks for a triplet" do
+      hand.cards = [card1, card2, card3, card4, card5]
+      expect(hand.triplet?).to be true
+    end
   end
 
   describe "#two_pair?" do
-    it "checks for a two pair"
+    let(:card1) { Card.new("K", :H)}
+    let(:card2) { Card.new("4", :H)}
+    let(:card3) { Card.new("4", :H)}
+    let(:card4) { Card.new("5", :H)}
+    let(:card5) { Card.new("5", :H)} 
+    it "checks for a two pair" do
+      hand.cards = [card1, card2, card3, card4, card5]
+      expect(hand.two_pair?).to be true
+    end
+
   end
 
   describe "#pair?" do
-    it "checks for a pair"
+    let(:card1) { Card.new("A", :H)}
+    let(:card2) { Card.new("8", :H)}
+    let(:card3) { Card.new("6", :H)}
+    let(:card4) { Card.new("5", :H)}
+    let(:card5) { Card.new("5", :H)} 
+  it "checks for a pair" do
+    hand.cards = [card1, card2, card3, card4, card5]
+    expect(hand.pair?).to be true
+  end
   end
   
 
