95db31f1d2a223c07ef91bb4b391843df8204c77
LeoC <leo.cheng92@gmail.com>
2022-01-18 14:50:08 -0500

    starting AA_questions SQLite3/Ruby project

---
+++ AA_Questions/import_db.sql
@@ -0,0 +1,103 @@
+PRAGMA foreign_keys = ON;
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+  id INTEGER PRIMARY KEY,
+  fname TEXT NOT NULL,
+  lname TEXT NOT NULL  
+);
+
+DROP TABLE IF EXISTS questions;
+
+CREATE TABLE questions (
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  body TEXT NOT NULL,
+  author_id INTEGER NOT NULL,
+
+  FOREIGN KEY (author_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS question_follows;
+
+CREATE TABLE question_follows (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+DROP TABLE IF EXISTS replies;
+
+CREATE TABLE replies (
+  id INTEGER PRIMARY KEY,
+  subject_id INTEGER NOT NULL,
+  parent_id INTEGER,
+  op_id INTEGER NOT NULL,
+  body NOT NULL,
+
+  FOREIGN KEY (subject_id) REFERENCES questions(id),
+  FOREIGN KEY (parent_id) REFERENCES replies(id),
+  FOREIGN KEY (op_id) REFERENCES users(id)
+);
+
+DROP TABLE IF EXISTS question_likes;
+
+CREATE TABLE question_likes (
+  id INTEGER PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  question_id INTEGER NOT NULL,
+
+
+  FOREIGN KEY (user_id) REFERENCES users(id),
+  FOREIGN KEY (question_id) REFERENCES questions(id)
+);
+
+INSERT INTO
+  users (fname, lname)
+VALUES 
+  ('Leo', 'Cheng'),
+  ('John', 'Smorg'),
+  ('Cat', 'Choi'),
+  ('Kendall', 'Roy');
+
+INSERT INTO
+  questions (title, body, author_id)
+VALUES
+  ('Help!', 'I can''t seem to fix my chess methods. Any ideas?', (SELECT id FROM users WHERE fname = 'Leo')),
+  ('Bowling?', 'Anybody want to hit the lanes next weekend?', (SELECT id FROM users WHERE fname = 'John')),
+  ('HOW TO INITIALIZE', 'PLEASE HELP I NEED HELP TO INITLIAZE PROJECT DUE TMRW I AM STRUGGLING', (SELECT id FROM users WHERE fname = 'Cat')),
+  ('I am the greatest person in history', 'This is fact.', (SELECT id FROM users WHERE fname = 'Kendall')),
+  ('Help!!', 'I have no idea how to do this', (SELECT id FROM users WHERE fname = 'Leo'));
+
+INSERT INTO
+  question_follows (user_id, question_id)
+VALUES 
+  ((SELECT id FROM users WHERE fname = 'Leo'), (SELECT id FROM questions WHERE title = 'Help!')),
+  ((SELECT id FROM users WHERE fname = 'Leo'), (SELECT id FROM questions WHERE title = 'Help!!')),
+  ((SELECT id FROM users WHERE fname = 'John'), (SELECT id FROM questions WHERE title = 'Bowling?')),
+  ((SELECT id FROM users WHERE fname = 'Cat'), (SELECT id FROM questions WHERE title = 'HOW TO INITIALIZE')),
+  ((SELECT id FROM users WHERE fname = 'Kendall'), (SELECT id FROM questions WHERE title = 'I am the greatest person in history')),
+  ((SELECT id FROM users WHERE fname = 'Leo'), (SELECT id FROM questions WHERE title = 'I am the greatest person in history')),
+  ((SELECT id FROM users WHERE fname = 'Cat'), (SELECT id FROM questions WHERE title = 'I am the greatest person in history'));
+
+INSERT INTO
+  replies (subject_id, op_id, body)
+VALUES
+  ((SELECT id FROM questions WHERE title = 'I am the greatest person in history'), (SELECT id FROM users WHERE fname = 'Leo'), 'uhh what?'), 
+  ((SELECT id FROM questions WHERE title = 'Bowling?'), (SELECT id FROM users WHERE fname = 'John'), 'anyone?');
+
+INSERT INTO
+  replies (subject_id, op_id, body, parent_id)
+VALUEs
+  ((SELECT id FROM questions WHERE title = 'I am the greatest person in history'), (SELECT id FROM users WHERE fname = 'Kendall'), 'You heard me', (SELECT id FROM replies WHERE body = 'uhh what?')),
+  ((SELECT id FROM questions WHERE title = 'I am the greatest person in history'), (SELECT id FROM users WHERE fname = 'Cat'), 'PLEASE HELP!!', (SELECT id FROM replies WHERE body = 'uhh what?'));
+
+INSERT INTO
+  question_likes (user_id, question_id)
+VALUES 
+  ((SELECT id FROM users WHERE fname = 'Leo'), (SELECT id FROM questions WHERE title = 'Bowling?')),
+  ((SELECT id FROM users WHERE fname = 'Kendall'), (SELECT id FROM questions WHERE title = 'I am the greatest person in history'));
