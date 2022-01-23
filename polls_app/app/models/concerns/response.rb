c430924fd57ded6d7ef043656f490f3848a46dc3
LeoC <leo.cheng92@gmail.com>
2022-01-23 18:03:31 -0500

    finished polls project!

--- polls_app/app/models/concerns/response.rb
+++ polls_app/app/models/concerns/response.rb
@@ -1,13 +1,41 @@
 class Response < ApplicationRecord
+  validate :not_duplicate_response, :cannot_answer_own_poll
   
   belongs_to :respondent,
     class_name: 'User',
     foreign_key: :user_id,
-    primary_key: :id
+    primary_key: :id 
 
   belongs_to :answer_choice,
     class_name: 'AnswerChoice',
     foreign_key: :answer_id,
     primary_key: :id
 
+  has_one :question,
+  through: :answer_choice,
+  source: :question
+
+  def sibling_responses
+    self.question.responses.where.not(id: self.id)
+  end
+
+  def response_already_answered?
+    self.sibling_responses.exists?(user_id: respondent.id)
+  end
+
+  def not_duplicate_response
+    if response_already_answered?
+      errors[:question] << 'has already been answered by this username'
+    end 
+  end
+
+  def authored_own_poll?
+    self.respondent.authored_polls.exists?(self.question.poll.id)
+  end
+
+  def cannot_answer_own_poll
+    if authored_own_poll?
+      errors[:poll] << 'cannot be answered by tue author'
+    end
+  end
 end
--- polls_app/app/models/question.rb
+++ polls_app/app/models/question.rb
@@ -12,4 +12,35 @@ class Question < ApplicationRecord
     primary_key: :id,
     dependent: :destroy    
 
+  has_many :responses,
+    through: :answerchoices,
+    source: :responses
+
+  def results
+    # N+1
+    # ---
+    # results = {}
+    # self.answerchoices.each do |choices|
+    #   results[choices.text] = choices.responses.count
+    # end
+    # results
+
+    # answers = self.answerchoices.includes(:responses)
+    # results = {}
+    # answers.each do |choices|
+    #   results[choices.text] = choices.responses.length
+    # end
+    # results
+
+    acs = self.answerchoices    
+      .select('answer_choices.text, COUNT(responses.id) AS num_responses')
+      .left_outer_joins(:responses)
+      .group('answer_choices.text')
+
+    results = {}
+    acs.each { |a| results[a.text] = a.num_responses }
+    results
+    
+  end
+
 end
