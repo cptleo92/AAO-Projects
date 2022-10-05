34b91f3cc4d2be36ef034fa07cd16e0d0963441b
LeoC <leo.cheng92@gmail.com>
2022-10-05 18:05:11 -0400

    finish cards page

--- mui-tutorial/src/App.js
+++ mui-tutorial/src/App.js
@@ -1,9 +1,30 @@
-import './App.css';
+import { Container, Grid, Typography } from "@mui/material";
+import "./App.css";
+import SearchAppBar from "./components/AppBar";
+import TourCard from "./components/TourCard";
+import cities from "./data.json";
 
 function App() {
   return (
     <div className="App">
-      
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
+        {cities.map((city) => (
+          <>
+            <Typography
+              variant="h4"
+              component="h2"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map((tour, idx) => <TourCard key={idx} tour={tour}/>)}
+            </Grid>
+          </>
+        ))}
+      </Container>
     </div>
   );
 }
